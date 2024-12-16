3d80391acbc3ca3e76081a0ca981459f16dfef50
pablif <nhz01530@educastur.es>
2024-12-16 17:26:07 +0100

    Update CommandExec-1.php
    borrado echo shell.exec

--- CommandExecution/CommandExec-1.php
+++ CommandExecution/CommandExec-1.php
@@ -26,7 +26,6 @@
   <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px";"center">
     <?php
     if(isset($_GET["username"])){
-      echo shell_exec($_GET["username"]);
       if($_GET["username"] == "Admin" && $_GET["password"] == "ufoundmypassword")
         echo "WELLDONE";
     }
