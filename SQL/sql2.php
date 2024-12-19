ae9ab6d1b7e4aa1bd378a573e64772732f86f7e0
pablif <nhz01530@educastur.es>
2024-12-19 16:39:43 +0100

    Update sql2.php

--- SQL/sql2.php
+++ SQL/sql2.php
@@ -34,7 +34,7 @@
 	} 
 	//echo "Connected successfully";
 	if(isset($_POST["submit"])){
-		$number = $_POST['number'];
+		$number = mysqli_real_escape_string($conn,$_POST['number']);
 		$query = "SELECT bookname,authorname FROM books WHERE number = $number"; //Int
 		$result = mysqli_query($conn,$query);
 
