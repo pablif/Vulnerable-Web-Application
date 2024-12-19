26e941f06cd03561e3d407ff803b396219e72e05
pablif <nhz01530@educastur.es>
2024-12-19 16:43:13 +0100

    Update sql3.php

--- SQL/sql3.php
+++ SQL/sql3.php
@@ -35,7 +35,7 @@
 	} 
 	//echo "Connected successfully";
 	if(isset($_POST["submit"])){
-		$number = $_POST['number'];
+		$number = intval($_POST['number']);
 		$query = "SELECT bookname,authorname FROM books WHERE number = '$number'"; //Is this same with the level 2?
 		$result = mysqli_query($conn,$query);
 
